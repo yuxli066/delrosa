a4adfb2f503c03b71afb0c820e0b0389e34a67c4
yuxli066 <yuxli066@gmail.com>
2022-05-19 11:16:10 -0700

    finally got gmail smtp relay server working

--- massage-website-backend/src/github.com/yuxli066/massage/app/handler/services.go
+++ massage-website-backend/src/github.com/yuxli066/massage/app/handler/services.go
@@ -1,10 +1,15 @@
 package handler
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/smtp"
+	"os"
+	"path/filepath"
 )
 
 // smtpServer data to smtp server
@@ -14,8 +19,8 @@ type smtpServer struct {
 }
 
 // service configs
-const fromEmail string = "delrosamassage1962@gmail.com"
-const sendinBlueSMTPKey string = "xsYUHp52OK0mE9SD" // hide this
+const fromEmail string = "paulli@delrosamassage.com" // hide this
+const password string = "DelRosaMassage1962"         // hide this
 
 // valid emails to email appointments to
 func getValidEmails() []string {
@@ -37,24 +42,84 @@ func GetHealthCheck(w http.ResponseWriter, r *http.Request) {
 func SendEmail(w http.ResponseWriter, r *http.Request) {
 	// Receiver email addresses
 	// Add client's email to receiver email too with confirmation
-	var toEmails []string = getValidEmails()
-	var emailServer = smtpServer{host: "smtp-relay.sendinblue.com", port: "587"} // using ssl not tls
+	// var toEmails []string = getValidEmails()
+	// var emailServer = smtpServer{host: "smtp-relay.gmail.com", port: "587"} // using ssl not tls
 
 	// Message.
 	message := []byte("This is a really unimaginative message, I know.")
 
 	// Authentication.
-	auth := smtp.PlainAuth("", fromEmail, sendinBlueSMTPKey, emailServer.host)
+	// auth := smtp.PlainAuth("", fromEmail, password, "smtp-relay.gmail.com")
 
-	// Sending email.
-	err := smtp.SendMail(emailServer.getServerAddress(), auth, fromEmail, toEmails, message)
-
+	c, err := smtp.Dial("smtp-relay.gmail.com:587")
 	if err != nil {
 		log.Println(err.Error())
-		respondError(w, http.StatusInternalServerError, "Email Send Error")
-		return
 	}
+	defer c.Close()
+
+	// send HELO/EHLO command to smtp server
+	if err = c.Hello("delrosamassage.com"); err != nil {
+		log.Println("HELO/EHLO ERROR:", err.Error())
+	}
+	// // send HELO/EHLO command to smtp server
+	// if err = c.Hello(""); err != nil {
+	// 	log.Println("HELO/EHLO ERROR:", err.Error())
+	// }
+
+	// add smtp certificate before making tls connection
+	certs := x509.NewCertPool()
+	workingDir, _ := os.Getwd()
+	pemData, err := ioutil.ReadFile(filepath.Join(workingDir, "src/github.com/yuxli066/massage/certs/tlsCert.crt"))
+	if err != nil {
+		log.Println(err)
+	}
+	certs.AppendCertsFromPEM(pemData)
+	config := &tls.Config{ServerName: "smtp-relay.gmail.com"}
+	config.RootCAs = certs
+	if err = c.StartTLS(config); err != nil {
+		log.Println("TLS ERROR:", err.Error())
+	}
+
+	// Send Mail
+	if err = c.Mail(fromEmail); err != nil {
+		log.Println("MAIL ERROR:", err.Error())
+	}
+	if err = c.Rcpt("yuxli066@gmail.com"); err != nil {
+		log.Println("RECEIPT ERROR:", err.Error())
+	}
+	emailData, err := c.Data()
+	if err != nil {
+		log.Println("DATA ERROR:", err.Error())
+	}
+	_, err = emailData.Write(message)
+	if err != nil {
+		log.Println("DATA WRITE ERROR:", err.Error())
+	}
+	err = emailData.Close()
+	if err != nil {
+		log.Println("DATA CLOSE ERROR:", err.Error())
+	}
+
+	// Authenticate SMTP server
+	// if err = c.Auth(auth); err != nil {
+	// 	log.Println("AUTHENTICATION ERROR:", err.Error())
+	// }
+
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// 	respondError(w, http.StatusInternalServerError, "Email Send Error")
+	// 	return
+	// }
 
 	fmt.Println("Email Sent!")
 	respondJSON(w, http.StatusOK, map[string]bool{"Email Sent": true})
+}
+
+func getHostName() string {
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return hostname
 }
