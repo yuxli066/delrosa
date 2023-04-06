74ac48fded1c40abb7eadfc4c637345f4397ee93
Leo Li <leo@DESKTOP-I422KLF>
2023-04-05 23:54:08 -0700

    added ssl services

--- massage-website-frontend/src/services/httpCommon.js
+++ massage-website-frontend/src/services/httpCommon.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api_port_number = '65535';
 const base = process.env.NODE_ENV !== "production" ? 
-                        `https://api.delrosamassage.co/api/` : `http://localhost:${api_port_number}/api/`;
+                        `http://localhost:${api_port_number}/api/` : 
+                        `https://api.delrosamassage.co/api/`;
 
 export default axios.create({
   baseURL: base,
