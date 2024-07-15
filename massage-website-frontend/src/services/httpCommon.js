b239d38b54e14e1d9f153cab71dd65a3c40f4b56
leoli7405@gmail.com <leoli7405@gmail.com>
2024-07-15 00:49:56 -0700

    added unused deployment file and removed port 8888 from url since we have migrated to k8 deployments

--- massage-website-frontend/src/services/httpCommon.js
+++ massage-website-frontend/src/services/httpCommon.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api_port_number = '65535';
-const prod_api_port_number = '8888'
 const base = process.env.NODE_ENV !== "production" ? 
                         `http://localhost:${api_port_number}/api/` : 
-                        `https://api.delrosamassage.co:${prod_api_port_number}/api/`;
+                        `https://api.delrosamassage.co/api/`;
 
 export default axios.create({
   baseURL: base,
