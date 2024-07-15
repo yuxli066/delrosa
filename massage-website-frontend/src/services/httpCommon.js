e808f2d9765b0794ca5e8e803bf2e38a63a25aca
leoli7405@gmail.com <leoli7405@gmail.com>
2024-07-15 00:56:20 -0700

    using just http for now, we're gonna have to add ssl to k3 after

--- massage-website-frontend/src/services/httpCommon.js
+++ massage-website-frontend/src/services/httpCommon.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api_port_number = '65535';
 const base = process.env.NODE_ENV !== "production" ? 
                         `http://localhost:${api_port_number}/api/` : 
-                        `https://api.delrosamassage.co/api/`;
+                        `http://api.delrosamassage.co/api/`;
 
 export default axios.create({
   baseURL: base,
