d2f32c06beb4602b18b5e39c3cefe2705334a9f3
leoli7405@gmail.com <leoli7405@gmail.com>
2024-07-22 12:01:29 -0700

    updated

--- massage-website-frontend/src/services/httpCommon.js
+++ massage-website-frontend/src/services/httpCommon.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api_port_number = '65535';
 const base = process.env.NODE_ENV !== "production" ? 
-                        `http://localhost:${api_port_number}/api/` : 
-                        `http://api.delrosamassage.co/api/`;
+                        `https://localhost:${api_port_number}/api/` : 
+                        `https://api.delrosamassage.co/api/`;
 
 export default axios.create({
   baseURL: base,
